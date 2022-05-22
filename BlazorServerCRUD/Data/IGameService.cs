94ca580e1a75b0f331f4d5e4ec27e45be20ed9be
KennedyMungai <kennedymungaifmab@gmail.com>
2022-05-22 11:23:08 +0300

    GetSingleGame,CreateGame, UpdateGame ,DeleteGame

--- BlazorServerCRUD/Data/IGameService.cs
+++ BlazorServerCRUD/Data/IGameService.cs
@@ -4,4 +4,8 @@ public interface IGameService
 {
     List<Game> Games {get; set;}
     Task LoadGames();
+    Task GetSingleGame(int id);
+    Task CreateGame(Game game);
+    Task UpdateGame(Game game, int id);
+    Task DeleteGame(int id);
 }
