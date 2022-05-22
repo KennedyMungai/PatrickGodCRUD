b31114a2e206fc21ab6878dcdd352760100521a8
KennedyMungai <kennedymungaifmab@gmail.com>
2022-05-22 10:49:10 +0300

    Added DbSet Games and ModelBuilder data

--- BlazorServerCRUD/Data/DataContext.cs
+++ BlazorServerCRUD/Data/DataContext.cs
@@ -4,8 +4,30 @@ namespace BlazorServerCRUD.Data;
 
 public class DataContext : DbContext
 {
+    public DbSet<Game> Games => Set<Game>();
+
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Game>().HasData(
+            new Game
+            {
+                Id=1, 
+                Name = "Half Life 2", 
+                Developer = "Valve", 
+                Release = new DateTime(2004,11,16)
+            },
+            new Game
+            {
+                Id=2, 
+                Name = "Day of the tentacle", 
+                Developer = "Lucas Arts", 
+                Release = new DateTime(1993,5,25)
+            }
+        );
     }
 }
